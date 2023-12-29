26c46e9dcc02928deb640cce7b6630cd10fd27c2
danh <deltronik98@gmail.com>
2023-12-29 02:05:42 -0300

    refactor: reescritura de la gran mayoria del codigo [espero para mejor]

--- assets/js/toggle_theme.js
+++
@@ -1,8 +0,0 @@
-const theme  = window.matchMedia('(prefers-color-scheme: dark)');
-if (theme.matches){
-document.getElementById('avatar').src="{{ $.Site.Params.header.avatar_dark }}";
-console.log("dark");
-}else{
-document.getElementById('avatar').src="{{ $.Site.Params.header.avatar_light }}";
-console.log("light");
-}
